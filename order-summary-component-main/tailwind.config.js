762e8b381198e41baa970150a5bb7148c8049e41
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-02 12:31:27 +0000

    finish

--- order-summary-component-main/tailwind.config.js
+++ order-summary-component-main/tailwind.config.js
@@ -1,7 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '376px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        "pale-blue": "#e0e8ff",
+        "bright-blue": "#3829e0",
+        "very-pale-blue": "#f5f7ff",
+        "desaturated-blue": "#7280a7",
+        "dark-blue": "#1f2f56",
+      },
+      fontFamily: {
+        "red-hat": "'Red Hat Display', sans-serif",
+      },
+    },
   },
   plugins: [],
 }
