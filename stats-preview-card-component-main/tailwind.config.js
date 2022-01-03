e3cf39e50d6efb4acba43d850e31aa52789ddc77
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-03 09:36:26 +0000

    implemented style guide

--- stats-preview-card-component-main/tailwind.config.js
+++ stats-preview-card-component-main/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-dark-blue": "#090b1a",
+        "dark-desaturated-blue": "#1b1938",
+        "soft-violet": "#aa5cdb",
+      },
+      fontFamily: {
+        "inter": "'Inter', sans-serif",
+        "lexend": "'Lexend Deca', sans-serif",
+      },
+    },
   },
   plugins: [],
 }
