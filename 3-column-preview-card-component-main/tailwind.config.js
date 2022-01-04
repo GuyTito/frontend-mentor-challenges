075e86d6e98882809eb2c703a25d4bbe3b4fc0fe
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-04 08:30:40 +0000

    implement style guide

--- 3-column-preview-card-component-main/tailwind.config.js
+++ 3-column-preview-card-component-main/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bright-orange": "#e38826",
+        "dark-cyan": "#006970",
+        "very-dark-cyan": "#004241",
+        "very-light-gray": "#f2f2f2",
+      },
+      fontFamily: {
+        "lexend": "'Lexend Deca', sans-serif",
+        "shoulders": "'Big Shoulders Display', cursive",
+      },
+    },
   },
   plugins: [],
 }
