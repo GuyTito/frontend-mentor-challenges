54c1d7fc1955951c923603d417cd6f93b045971d
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-04 14:48:45 +0000

    implemented style guide

--- faq-accordion-card-main/tailwind.config.js
+++ faq-accordion-card-main/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-dark-desaturated-blue": "#1d1e35",
+        "soft-red": "#f47c57",
+        "soft-violet": "#af67e9",
+        "soft-blue": "#6565e7",
+        "very-dark-grayish-blue": "#4a4b5e",
+        "dark-grayish-blue": "#787887",
+        "light-grayish-blue": "#e7e7e9",
+      },
+      fontFamily: {
+        "kumbh": "'Kumbh Sans', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
