38d6a4ea2859de2b8bb107fd8387c9e926ef8765
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-07 12:29:23 +0000

    implemented style guide

--- intro-component-with-signup-form-master/tailwind.config.js
+++ intro-component-with-signup-form-master/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red-primary": "hsl(0, 100%, 74%)",
+        "green-primary": "hsl(154, 59%, 51%)",
+        "blue-accent": "hsl(248, 32%, 49%)",
+        "dark-blue": "hsl(249, 10%, 26%)",
+        "grayish-blue": "hsl(246, 25%, 77%)",
+      },
+      fontFamily: {
+        "poppins": "'Poppins', sans-serif",
+      },
+    },
   },
   plugins: [],
 }
