a1be8186993cf4cd0825ed2d0f5ea34e3bcc246b
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-09 09:18:38 +0000

    implemented style guide

--- four-card-feature-section-master/tailwind.config.js
+++ four-card-feature-section-master/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins": " 'Poppins', sans-serif",
+      },
+      colors: {
+        "red-primary": "hsl(0, 78%, 62%)",
+        "cyan-primary": "hsl(180, 62%, 55%)",
+        "orange-primary": "hsl(34, 97%, 64%)",
+        "blue-primary": "hsl(212, 86%, 64%)",
+        "very-dark-blue": "hsl(234, 12%, 34%)",
+        "grayish-blue": "hsl(229, 6%, 66%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 }
