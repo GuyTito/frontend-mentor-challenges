b80aac4f8cdbc24d69c5f1380a0de1f6b631ee04
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-11 08:35:14 +0000

    implemented style guide

--- tip-calculator-app-main/tailwind.config.js
+++ tip-calculator-app-main/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Strong-cyan": "hsl(172, 67%, 45%)",
+        "Very-dark-cyan": "hsl(183, 100%, 15%)",
+        "Dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "Dark-grayish-cyan": "hsl(184, 14%, 56%)",
+        "Light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "Light-grayish-cyan": "hsl(189, 41%, 97%)",
+      },
+      fontFamily: {
+        "space-mono": "'Space Mono', monospace",
+      },
+    },
   },
   plugins: [],
 }
