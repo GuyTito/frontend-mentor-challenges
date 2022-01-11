d4793738ae384fd117e3bfbf857aacc53668cc88
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-11 12:21:18 +0000

    work in progress

--- tip-calculator-app-main/tailwind.config.js
+++ tip-calculator-app-main/tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
       colors: {
         "Strong-cyan": "hsl(172, 67%, 45%)",
         "Very-dark-cyan": "hsl(183, 100%, 15%)",
-        "Dark-grayish-cyan": "hsl(186, 14%, 43%)",
-        "Dark-grayish-cyan": "hsl(184, 14%, 56%)",
-        "Light-grayish-cyan": "hsl(185, 41%, 84%)",
-        "Light-grayish-cyan": "hsl(189, 41%, 97%)",
+        "Dark-grayish-cyan-1": "hsl(186, 14%, 43%)",
+        "Dark-grayish-cyan-2": "hsl(184, 14%, 56%)",
+        "Light-grayish-cyan-1": "hsl(185, 41%, 84%)",
+        "Light-grayish-cyan-2": "hsl(189, 41%, 97%)",
       },
       fontFamily: {
         "space-mono": "'Space Mono', monospace",
