c35bf17c1f5f7ae27f8d60b5822550c9f2ae99e8
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-14 11:23:19 +0000

    implement style guide

--- 9. sunnyside-agency-landing-page-main/tailwind.config.js
+++ 9. sunnyside-agency-landing-page-main/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Soft-red": "hsl(7, 99%, 70%)",
+        "Yellow-primary": "hsl(51, 100%, 49%)",
+        "Dark-desaturated-cyan": "hsl(167, 40%, 24%)",
+        "Dark-blue": "hsl(198, 62%, 26%)",
+        "Dark-moderate-cyan": "hsl(168, 34%, 41%)",
+        "Very-dark-desaturated-blue": "hsl(212, 27%, 19%)",
+        "Very-dark-grayish-blue": "hsl(213, 9%, 39%)",
+        "Dark-grayish-blue": "hsl(232, 10%, 55%)",
+        "Grayish-blue": "hsl(210, 4%, 67%)",
+      },
+      fontFamily: {
+        "Barlow": "'Barlow', sans-serif",
+        "Fraunces": "'Fraunces', serif",
+      },
+    },
   },
   plugins: [],
 }
