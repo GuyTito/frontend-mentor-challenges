d31da01bb6cb1ff4dd334e210e6747d0f1ce2af0
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-01-22 22:40:29 +0000

    implement style guide colors and font family

--- 10_crowdfunding-product-page-main/tailwind.config.js
+++ 10_crowdfunding-product-page-main/tailwind.config.js
@@ -1,7 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Moderate-cyan": "hsl(176, 50%, 47%)",
+        "Dark-cyan": "hsl(176, 72%, 28%)",
+        "Dark-gray": "hsl(0, 0%, 48%)",
+      },
+      fontFamily: {
+        serif : ["'Commissioner'", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
